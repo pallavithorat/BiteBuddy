df038ff9d32534e52d7e493111db7683bb33b5bd
pallavithorat <thoratpallavi2000@gmail.com>
2024-08-21 22:21:17 -0700

    Added Tailwind CSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -47,18 +47,18 @@ const Body = () => {
     <Shimmer />
     ):(
     <div className="body">
-      <div className="filter">
+      <div className="filter flex">
 
-      <div className="search">
+      <div className="search m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
-          <button
+          <button className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               // Filter the restraunt cards and update the UI
               // searchText
@@ -75,9 +75,9 @@ const Body = () => {
           </button>
         </div>
 
-
-        <button
-          className="filter-btn"
+        <div className="search m-4 p-4 flex items-center">
+        <button 
+          className="px-4 py-2 bg-gray-100 rounded-lg"
           onClick={() => { //Its a callback function , this fun will be call after clicking on that button
             const filteredList = listOfRestaurants.filter( //As soon as my state variable(listOfRestaurants) changes it(hooks) will automatically refresh our component
               (res) => res.info.avgRating > 4
@@ -87,8 +87,10 @@ const Body = () => {
         >
           Top Rated Restaurants
         </button>
+
       </div>
-      <div className="res-container">
+      </div>
+      <div className="flex flex-wrap ">
         {filteredRestaurant.map((restaurant) => (
          <Link
          key={restaurant.info.id}
