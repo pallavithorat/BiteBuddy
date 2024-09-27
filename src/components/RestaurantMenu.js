58f1d84aa0fb1e4fe7b1b9566aa99eb27d80dea2
pallavithorat <thoratpallavi2000@gmail.com>
2024-09-27 15:55:04 -0700

    Menu display fixed

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -27,33 +27,39 @@ const RestaurantMenu = () => {
 
   console.log(itemCards);
 
-  const categories =
-  resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card.filter(
-    (c) =>
-      c.cta?.["cta"]?.["link"] ===
-      "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-  );
+  // const categories =
+  // resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card.filter(
+  //   (c) =>
+  //     c.cta?.["cta"]?.["link"] ===
+  //     "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  // );
 
-  console.log(categories);
+  const categories = resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory" )
+    console.log(categories);
+    
 
   return (
       <div className="text-center">
+
       <h1 className="font-bold my-6 text-2xl">{name}</h1>
       <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      <h2>Menu</h2>
-      <ul>
+    
+
+      {/* <ul>
         {itemCards.map((item) => (
           <li key={item.card.info.id}>
             {item.card.info.name} -{" Rs."}
             {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
           </li>
         ))}
-      </ul>
+      </ul> */}
+
+  
 
        {/* categories accordions */}
-     //  {categories.map((category, index) => (
+       {categories.map((category, index) => (
         // controlled component
         <RestaurantCategory
           key={category?.card?.card.title}
@@ -63,6 +69,7 @@ const RestaurantMenu = () => {
           dummy={dummy}
         />
       ))}
+
     </div>
   );
 };
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -9,12 +9,14 @@ const useRestaurantMenu = (resId) => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(MENU_API + resId);
+    const data = await fetch(`${MENU_API}${resId}`);
     const json = await data.json();
-    setResInfo(json.data);
+    setResInfo(json?.data);
   };
 
   return resInfo;
 };
 
 export default useRestaurantMenu;
+
+// await fetch(MENU_API + resId)
